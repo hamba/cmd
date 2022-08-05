17d37b24c894696a6bd6685c4aa4fceb3e42ca49
Nicholas Wiersma <nick@wiersma.co.za>
2022-08-05 20:45:17 +0200

    chore: support go 1.19 (#125)

--- doc.go
+++ doc.go
@@ -4,6 +4,7 @@ Package cmd implements cmd helpers.
 This provides helpers on top of `github.com/urfave/cli`.
 
 Example usage:
+
 	var c *cli.Context // Get this from your action
 
 	log, err := cmd.NewLogger(c)
