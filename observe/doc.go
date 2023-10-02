227e26f745840c9335bf19efdd217f9b98f375cd
Nicholas Wiersma <nick@wiersma.co.za>
2023-10-02 21:37:04 +0200

    fix: formatting

--- observe/doc.go
+++ observe/doc.go
@@ -3,27 +3,27 @@ Package observe implements a type that combines statter, logger and tracer.
 
 Example usage:
 
-	func newObserver(c *cli.Context) (*observe.Observer, error) {
-	    log, err := cmd.NewLogger(c)
-	    if err != nil {
-	    	return nil, err
-	    }
+		func newObserver(c *cli.Context) (*observe.Observer, error) {
+		    log, err := cmd.NewLogger(c)
+		    if err != nil {
+		    	return nil, err
+		    }
 
-	    stats, err := cmd.NewStatter(c, log)
-	    if err != nil {
-	    	return nil, err
-	    }
+		    stats, err := cmd.NewStatter(c, log)
+		    if err != nil {
+		    	return nil, err
+		    }
 
-	    tracer, err := cmd.NewTracer(c, log,
-	    	semconv.ServiceNameKey.String("my-service"),
-	    	semconv.ServiceVersionKey.String("1.0.0"),
-	    )
-	    if err != nil {
-	    	return nil, err
-	    }
-	    tracerCancel := func() { _ = tracer.Shutdown(context.Background()) }
+		    tracer, err := cmd.NewTracer(c, log,
+		    	semconv.ServiceNameKey.String("my-service"),
+		    	semconv.ServiceVersionKey.String("1.0.0"),
+		    )
+		    if err != nil {
+		    	return nil, err
+		    }
+		    tracerCancel := func() { _ = tracer.Shutdown(context.Background()) }
 
-	    return observe.New(log, stats, tracer, tracerCancel), nil
-    }
+		    return observe.New(log, stats, tracer, tracerCancel), nil
+	    }
 */
 package observe
